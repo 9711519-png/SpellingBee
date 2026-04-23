b1b5129d8d03559b712e90c70b3384f828afa8a6
9711519-png <9711519@graniteschools.org>
2026-04-23 12:07:20 -0600

    Update GameController.java

--- src/GameController.java
+++ src/GameController.java
@@ -1,1 +1,19 @@
+public class GameController {
 
+    private WordBank wordbank;
+    private DifficultyMaager difficultyManager;
+    private Score Manager scoreManager;
+    private TTS tts;
+    private Timer timer;
+    private UI ui;
+
+    public GameController() {
+      //make objects later
+    }
+
+    public void startGame() {
+        //start game logic
+    }
+
+    public void startRound() {
+        // get word, speak it, 
