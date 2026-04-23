685bbde16c65e4c60fb56dfc1c730a87aafbe576
9711519-png <9711519@graniteschools.org>
2026-04-23 12:35:29 -0600

    Update Timer.java

--- src/Timer.java
+++ src/Timer.java
@@ -1,1 +1,21 @@
+public class Timer {
 
+  private int duration;
+  private int timeLeft:
+
+  public Timer(int duration) {
+      this.duration = duration;
+  }
+
+  public void start() {
+      // start countdown
+  }
+
+  public void stop() {
+    // stoptimer
+  }
+
+  public void reset() {
+    // reset timer
+  }
+}
